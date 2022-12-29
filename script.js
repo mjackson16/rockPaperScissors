1208b23d59436e0b6486ddfc21a6cd9b58eb887f
Matthew Jackson <mjackson2094@gmail.com>
2022-12-29 15:12:55 -0500

    Add playRound function to script.js

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
-  let choices = ['Rock', 'Paper', 'Scissors'];
+  let choices = ['Rock', 'Raper', 'Scissors'];
   return choices[choice];
+}
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection =
+    playerSelection.charAt(0).toUpperCase() +
+    playerSelection.slice(1).toLowerCase();
+
+  if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
+    return 'You win! Rock beats Scissors';
+  } else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
+    return 'You win! Paper beats Rock';
+  } else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
+    return 'You win! Paper beats Rock';
+  } else if (playerSelection == computerSelection) {
+    return 'No winner! It is a draw';
+  } else {
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
+  }
 }
