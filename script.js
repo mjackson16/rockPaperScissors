3a69ff1e430891a1e60f0e9ff1e016a01a9afaa1
Matthew Jackson <mjackson2094@gmail.com>
2022-12-29 16:03:19 -0500

    Add game and capitalize function to script.js. Rock Paper Scissors fully playable in console.

--- script.js
+++ script.js
@@ -4,20 +4,73 @@ function getComputerChoice() {
   return choices[choice];
 }
 
+function capitalize(x) {
+  x = x.charAt(0).toUpperCase() + x.slice(1).toLowerCase();
+  return x;
+}
+
 function playRound(playerSelection, computerSelection) {
   playerSelection =
     playerSelection.charAt(0).toUpperCase() +
     playerSelection.slice(1).toLowerCase();
 
   if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
-    return 'You win! Rock beats Scissors';
+    console.log('You win! Rock beats Scissors');
+    return 1;
   } else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
-    return 'You win! Paper beats Rock';
+    console.log('You win! Paper beats Rock');
+    return 1;
   } else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
-    return 'You win! Paper beats Rock';
+    console.log('You win! Paper beats Rock');
+    return 1;
   } else if (playerSelection == computerSelection) {
-    return 'No winner! It is a draw';
+    console.log('No winner! It is a draw');
+    return 0;
   } else {
-    return `You lose! ${computerSelection} beats ${playerSelection}`;
+    console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+    return -1;
+  }
+}
+
+function game() {
+  let round = 1;
+  let playerScore = 0;
+  let computerScore = 0;
+  while (round < 6) {
+    let playerSelection = prompt('Rock, Paper, or Scissors?');
+    playerSelection = capitalize(playerSelection);
+
+    while (
+      playerSelection !== 'Rock' &&
+      playerSelection !== 'Paper' &&
+      playerSelection !== 'Scissors'
+    ) {
+      playerSelection = prompt('Not a valid choice. Rock, Paper, or Scissors?');
+      playerSelection = capitalize(playerSelection);
+    }
+
+    let point = playRound(playerSelection, getComputerChoice());
+
+    switch (point) {
+      case 1:
+        playerScore++;
+        break;
+      case -1:
+        computerScore++;
+        break;
+      default:
+        break;
+    }
+
+    round++;
+  }
+  console.log(`Player Score: ${playerScore}`);
+  console.log(`Computer Score: ${computerScore}`);
+  if (playerScore > computerScore) {
+    console.log('Player wins!');
+  } else if (playerScore == computerScore) {
+    console.log("It's a tie! No winner.");
+  } else {
+    console.log('Computer wins!');
   }
 }
